414fb9829fd1627d9c5d99bde264345f409db9d8
Venkatvemulapati <96244297+Venkatvemulapati@users.noreply.github.com>
2022-05-02 07:40:41 +0530

    Add files via upload

---
+++ Assignment_3/Assignment_3/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ namespace Assignment_3
+{
+    class  Student
+    {
+        int rollno;
+        string name;
+        int year;
+        int semester;
+        string branch;
+        
+        public Student(int rollno,string name,int year,int semester,string branch)
+        {
+            this.rollno = rollno;
+            this.name = name;
+            this.year = year;
+            this.semester = semester; 
+            this.branch = branch;
+            this.marks = marks;
+
+        }
+        public void displayResult
+        {
+            int[] marks = new int[5];
+            marks[0] = 29;
+            marks[1] = 49;
+            marks[2] = 41;
+            marks[3] = 50;
+            marks[4] = 90;
+            public void displayStudentResult()
+        {
+            int[] marks = new int[5];
+            marks[0] = 29;
+            marks[1] = 49;
+            marks[2] = 41;
+            marks[3] = 50;
+            marks[4] = 90;
+
+            for (int i = 0; i < marks.Length; i++)
+            {
+                Console.WriteLine(marks[i]);
+            }
+
+            double average = marks.Average();
+            Console.WriteLine("The total subject of average : " + average);
+
+            for (int i = 0; i < marks.Length; i++)
+            {
+
+                if (marks[i] < 35)
+                {
+                    Console.WriteLine(marks[i] + " subject is failed");
+                }
+                else if (marks[i] > 35 && average < 50)
+                {
+                    Console.WriteLine("Student result is failed");
+                }
+                else if (average > 50)
+                {
+                    Console.WriteLine("student result is passed ");
+                }
+                else
+                {
+                    Console.WriteLine("Please enter student valid details");
+                }
+            }
+
+        }
+
+        public Student(int rollno, String name, string stclass, string semister, string branch)
+        {
+            this.rollno = rollno;
+            this.name = name;
+            this.stclass = stclass;
+            this.semister = semister;
+            this.branch = branch;
+            Console.WriteLine($"Rollno is:{rollno}, Name is{name},Student class is{stclass}, Semister is{semister},Branch is :{branch}");
+
+        }
+        public void displayStudentData()
+        {
+            Console.WriteLine("-------------------The result is----------------------------");
+
+        }
+    }
+}
+        
+
+    
