c29631bd6061fefb97fe2ee34a0b80bcea0a1cb5
Venkatvemulapati <96244297+Venkatvemulapati@users.noreply.github.com>
2022-05-05 10:11:14 +0530

    Add files via upload

---
+++ CodeBasedQuestion/CodeBasedQuestion/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace CodeBasedQuestion
+{
+    internal class TrainTicket
+    {
+        class Passenger
+        {
+            public String Name { get; set; }
+            public int Age { get; set; }
+            Ticket Ticket { get; set; }
+            public String Date { get; set;}
+
+
+            public Passenger(string name, int age ,string date)
+            {
+                Name = name;
+                Age = age;
+                DateOfJourney = date;
+                Ticket = new Ticket();
+
+            }
+            public void TicketBooking(int tickets)
+            {
+                Ticket.Tickets = tickets;
+                Console.WriteLine("Ticket booked successfully");
+            }
+        }
+        class Ticket
+        {
+            public int Tickets;
+            public int tickets
+            {
+                get => tickets;
+                set
+                {
+
+
+
+                    if (value > 5)
+
+                        throw new Exception("One time you can book only less than 5 tickets");
+
+
+                    else
+
+                        tickets = value;
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Date of journey");
+            String Date = Console.ReadLine();
+            Passenger passenger = new Passenger("sai", 24,Date);
+            Console.WriteLine("Enter the number of tickets want to book");
+            passenger.TicketBooking(int.Parse(Console.ReadLine()));
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
+
