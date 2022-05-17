250cc3c354f6be404dc50c9c78909d9c2447cde8
Venkatvemulapati <96244297+Venkatvemulapati@users.noreply.github.com>
2022-05-17 18:30:53 +0530

    Add files via upload

---
+++ CodeBaseTest2/CodeBaseTest2/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CalculatorApp
+{
+    class Program
+    {
+        public delegate void addnum(int a, int b);
+        public delegate void subnum(int a, int b);
+        public delegate void divnum(int a, int b);
+        public delegate void mulnum(int a, int b);
+
+
+        public void sum(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void subtract(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+        public void multiplication(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+        public void divison(int a, int b)
+        {
+            Console.WriteLine(a / b);
+        }
+
+        public static void Main()
+        {
+            Program app = new Program();
+            addnum sum1 = new addnum(app.sum);
+            subnum sub1 = new subnum(app.subtract);
+            divnum div1 = new divnum(app.divison);
+            mulnum mul1 = new mulnum(app.multiplication);
+        }
+    }
+}
+            
