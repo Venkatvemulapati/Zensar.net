5adfe3d8b3283fbd4f93f875b267aeec96d2c89a
Venkatvemulapati <96244297+Venkatvemulapati@users.noreply.github.com>
2022-06-16 19:00:49 +0530

    Add files via upload

---
+++ codebase4.aspx
@@ -0,0 +1,24 @@
+﻿<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<title></title>
+</head>
+<body style="height: 321px">
+<form id="form1" runat="server">
+<div>
+SELECT MOBILE<asp:DropDownList ID="DropDownList1" runat="server" Height="35px" Width="150px" style="margin-left: 28px" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+</asp:DropDownList>
+<br />
+
+
+
+</div>
+<asp:Image ID="Image1" runat="server" Height="151px" style="margin-left: 63px; margin-top: 36px" Width="135px" />
+<br />
+<br />
+<br />
+<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+<br />
+<asp:Button ID="button1" runat="server" Text="CLICK FOR PRICE" OnClick="button1_Click" />
+</form>
+</body>
+</html>
---
+++ codebase4.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication16
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string[] str = new string[] { "select", "Iphone", "samsung", "redmi" };
+                for (int i = 0; i < str.Length; i++)
+                {
+                    DropDownList1.Items.Add(str[i]);
+                }
+            }
+
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string str = DropDownList1.Text;
+            Image1.ImageUrl = "~Content/pictures/Images" + str + ".jfif";
+
+
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged1(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void button1_Click(object sender, EventArgs e)
+        {
+            if (DropDownList1.Items.FindByText("redmi").Selected == true)
+            {
+                TextBox1.Text = "Price: 15000";
+            }
+            else if (DropDownList1.Items.FindByText("samsung").Selected == true)
+            {
+                TextBox1.Text = "Price: 20000";
+            }
+            else if (DropDownList1.Items.FindByText("iphone").Selected == true)
+            {
+                TextBox1.Text = "Price:30000";
+            }
+
+        }
+    }
+}
